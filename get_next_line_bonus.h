ab9f73117f761dfd60b80fdb485fc4649232b407
Oldtown Britany <obritany@am-d1.msk.21-school.ru>
2021-01-27 13:14:46 +0300

    first

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obritany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/24 19:55:59 by obritany          #+#    #+#             */
+/*   Updated: 2020/12/24 19:56:04 by obritany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+int		get_next_line(int fd, char **line);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obritany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/24 19:56:20 by obritany          #+#    #+#             */
+/*   Updated: 2020/12/24 19:56:22 by obritany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (0);
+	while (*s && (*s != (char)c))
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t len;
+
+	if (!s)
+		return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *rslt;
+	char *temp;
+
+	if (!s1 || !s2)
+		return (0);
+	rslt = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (rslt == 0)
+		return (0);
+	temp = rslt;
+	while (*s1)
+	{
+		*temp = *s1;
+		temp++;
+		s1++;
+	}
+	while (*s2)
+	{
+		*temp = *s2;
+		temp++;
+		s2++;
+	}
+	*temp = '\0';
+	return (rslt);
+}
+
+char	*ft_strdup(const char *str)
+{
+	ssize_t	i;
+	char	*dest;
+
+	if (!str)
+		return (0);
+	dest = malloc(sizeof(*dest) * (ft_strlen(str) + 1));
+	if (dest == 0)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
